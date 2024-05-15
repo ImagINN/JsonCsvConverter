3ef896940bad3f139f4b4da138a98755fb692bd4
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-05-16 01:52:47 +0300

    feat: Update JavaScript file references for downloading files.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using JsonCsvConverter;
-using Microsoft.Extensions.DependencyInjection;
-using CsvHelper;
-using Newtonsoft.Json;
-using System.IO;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
---
+++ wwwroot/js/downloadJsonFile.js
@@ -0,0 +1,12 @@
+function downloadJsonFile(filename, content) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(content));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
